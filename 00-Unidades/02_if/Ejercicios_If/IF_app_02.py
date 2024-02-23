d6a3b9898ce1a30401459b4b105f2bf70040f627
ValeriaGonzalez07 <valeria.agp.12@gmail.com>
2024-02-23 19:01:37 -0300

    Resolvi ejercicio IF 02

--- 00-Unidades/02_if/Ejercicios_If/IF_app_02.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Valeria Andreina
+apellido: Gonzalez Perez
 ---
 Ejercicio: if_02
 ---
@@ -33,7 +33,11 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        edad_usuario = int(self.txt_edad.get())
+        if edad_usuario > 17:
+            alert("Situacion Legal", "Usted es MAYOR")
+        else:
+            alert("Situacion Legal"," ")
 
         
         
