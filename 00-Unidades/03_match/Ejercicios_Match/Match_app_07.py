2e09e88323108626104a19ede99efb653c710b16
ValeriaGonzalez07 <valeria.agp.12@gmail.com>
2024-02-25 20:18:56 -0300

    Resolvi ejercicio Match 07

--- 00-Unidades/03_match/Ejercicios_Match/Match_app_07.py
+++ 00-Unidades/03_match/Ejercicios_Match/Match_app_07.py
@@ -6,8 +6,8 @@ import customtkinter
 
 
 '''
-nombre:
-apellido:
+nombre: Valeria Andreina
+apellido: Gonzalez Perez
 ---
 Ejercicio: Match_07
 ---
@@ -34,7 +34,16 @@ class App(customtkinter.CTk):
         
     
     def btn_informar_on_click(self):
-        pass
+        destino = (self.combobox_destino.get())
+        match destino :
+            case "Bariloche" :
+                alert("Coordenada geografica","Sur - Oeste")
+            case "Cataratas" :
+                alert("Coordenada geografica","Norte")
+            case "Mar del plata" :
+                alert("Coordenada geografica","Sur - Este")
+            case "Ushuaia" :
+                alert("Coordenada geografica","Sur")
     
     
 if __name__ == "__main__":
