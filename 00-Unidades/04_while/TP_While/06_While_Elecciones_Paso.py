b403282d92d3c303827b591a2b72bb8891d1ed0e
ValeriaGonzalez07 <valeria.agp.12@gmail.com>
2024-02-27 22:05:41 -0300

    Resolvi TP 06 While

--- 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
+++ 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
@@ -35,7 +35,61 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        nombre_ganador = " "
+        edad_promedio = 0
+        total_votos = 0
+        nombre_perdedor = " "
+        edad_perdedor = 0
+        votos_ganador = 0
+        votos_perdedor = 0
+        condicional = 0
+        contador = 0
+        control = 0
+        continuar = "s"
+        while continuar == "s" or continuar == "S" :
+            contador = contador + 1
+            nombre = prompt("Nombre ","Introduzca el nombre del candidato")
+        
+            edad = int(prompt("Edad"," Introduzca edad del candidato"))
+            while edad < 24 :
+                alert("Dato Incorrecto","La edad del candidato debe ser mayor o igual a 25 años")
+                edad =int(prompt("Edad","Introduzca edad del candidato"))
+                      
+            votos = int(prompt("Votos","Introduzca la cantidad de votos"))
+            if votos < 0 :
+                while votos < 0 :
+                    alert("Dato Incorrecto","La cantidad de votos debe ser mayor o igual a cero")
+                    votos =int(prompt("Votos","Introduzca la cantidad de votos"))
+                    if condicional == 0 :
+                        votos_perdedor = votos
+                        edad_perdedor = edad
+                        condicional = 1 
+            else :
+                if control == 0 :
+                    votos_perdedor = votos
+                    edad_perdedor = edad
+                    nombre_perdedor = nombre
+                    control = 1
+                if votos > votos_ganador :
+                    nombre_ganador = nombre
+                if votos < votos_perdedor :
+                    votos_perdedor = votos
+                    edad_perdedor = edad
+                    nombre_perdedor = nombre
+
+            edad_promedio = edad_promedio + edad
+            total_votos = total_votos + votos
+            continuar = prompt("Desea continuar","S/N")
+                                    
+
+        edad_promedio = edad_promedio/contador
+        edad_perdedor = str(edad_perdedor)
+        total_votos = str(total_votos)
+        edad_promedio = str(edad_promedio)
+        alert ("Mayor cantidad de votos", "El ganador es: "+ nombre_ganador )
+        alert ("Menos Votos"," EL candidato con menos votos es: "+ nombre_perdedor + " con una edad de: " +edad_perdedor)
+        alert ("Promedio","El promedio de edades es de: "+ edad_promedio + " años")
+        alert ("Cantidad", "La cantidad total de votos es: "+ total_votos)
 
 
 if __name__ == "__main__":
